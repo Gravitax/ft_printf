7060b80e179689dbbe4fc51d800dfe5fafc9641b
Gravitax <36345089+Gravitax@users.noreply.github.com>
2019-08-27 23:31:51 +0200

    Update type.c

--- srcs/type.c
+++ srcs/type.c
@@ -12,32 +12,45 @@
 
 #include "../includes/ft_printf.h"
 
-static void		pf_handle_unsigned(t_printf *data)
+static void		handle_base(t_printf *data, uintmax_t nb)
 {
-	uintmax_t	nb;
-
-	if (data->i_conv == L)
-		nb = va_arg(data->list, unsigned long);
-	else if (data->i_conv == LL)
-		nb = va_arg(data->list, unsigned long long);
-	else if (data->i_conv == J)
-		nb = va_arg(data->list, uintmax_t);
-	else if (data->i_conv == Z)
-		nb = va_arg(data->list, size_t);
-	else
-		nb = va_arg(data->list, unsigned int);
-	if (data->i_conv == HH)
-		nb = (unsigned char)nb;
-	else if (data->i_conv == H)
-		nb = (unsigned short)nb;
 	if (data->conversion == 'o')
 		data->str = ft_uitoa_base(data, nb, 8);
 	else if (data->conversion == 'u')
 		data->str = ft_uitoa_base(data, nb, 10);
-	else
+	else if (data->str == 'X')
 		data->str = ft_uitoa_base(data, nb, 16);
-	if (data->conversion == 'x')
+	else
+	{
+		data->str = ft_uitoa_base(data, nb, 16);
 		pf_strtolower(data->str);
+	}
+}
+
+static void		pf_handle_unsigned(t_printf *data)
+{
+	uintmax_t	nb;
+
+	if (data->i_conv)
+	{
+		if (data->i_conv == L)
+			nb = va_arg(data->list, unsigned long);
+		else if (data->i_conv == LL)
+			nb = va_arg(data->list, unsigned long long);
+		else if (data->i_conv == J)
+			nb = va_arg(data->list, uintmax_t);
+		else if (data->i_conv == Z)
+			nb = va_arg(data->list, size_t);
+		else
+			nb = va_arg(data->list, unsigned int);
+		if (data->i_conv == HH)
+			nb = (unsigned char)nb;
+		else if (data->i_conv == H)
+			nb = (unsigned short)nb;
+	}
+	else
+		nb = va_arg(data->list, unsigned int);
+	handle_base(data, nb);
 }
 
 static void		pf_handle_ps(t_printf *data, char c)
@@ -62,24 +75,29 @@ static void		pf_handle_signed(t_printf *data)
 {
 	intmax_t	nb;
 
-	if (data->i_conv == L)
-		nb = va_arg(data->list, long);
-	else if (data->i_conv == LL)
-		nb = va_arg(data->list, long long);
-	else if (data->i_conv == J)
-		nb = va_arg(data->list, intmax_t);
-	else if (data->i_conv == Z)
-		nb = va_arg(data->list, size_t);
+	if (data->i_conv)
+	{
+		if (data->i_conv == L)
+			nb = va_arg(data->list, long);
+		else if (data->i_conv == LL)
+			nb = va_arg(data->list, long long);
+		else if (data->i_conv == J)
+			nb = va_arg(data->list, intmax_t);
+		else if (data->i_conv == Z)
+			nb = va_arg(data->list, size_t);
+		else
+			nb = va_arg(data->list, int);
+		if (data->i_conv == HH)
+			nb = (char)nb;
+		else if (data->i_conv == H)
+			nb = (short)nb;
+	}
 	else
 		nb = va_arg(data->list, int);
-	if (data->i_conv == HH)
-		nb = (char)nb;
-	else if (data->i_conv == H)
-		nb = (short)nb;
 	if (data->conversion == 'c')
 	{
 		data->str = (char *)pf_memalloc(sizeof(char) * 2);
-		*(data->str) = (char)nb;
+		*(data->str) = nb;
 	}
 	else
 		data->str = ft_itoa_base(data, nb, 10);
